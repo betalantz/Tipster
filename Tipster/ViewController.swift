d6ae965a1b9297cd06fd81e3baac2b9d3b091397
betalantz <betalantz@gmail.com>
2017-09-11 15:31:43 -0700

    BillAmt building correctly, adding tip slider

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -12,10 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet var numbButtonCollection: [UIButton]!
     @IBOutlet weak var billAmt: UILabel!
+    @IBOutlet var TipPercCollection: [UILabel]!
+    
+    var billAmount = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        numbButtonCollection.sort { $0.tag < $1.tag }
+        TipPercCollection.sort { $0.tag < $1.tag }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,7 +30,28 @@ class ViewController: UIViewController {
 
     @IBAction func numbButtonPressed(_ sender: UIButton) {
         print("Number \(sender.tag) pressed")
+        let addition = numbButtonCollection[sender.tag].titleLabel?.text
+        
+        let build: String = billAmount + addition!
+        print(build)
+        let newBill = Double(build)
+        if newBill!.truncatingRemainder(dividingBy: 1.0) != 0.0 {
+            let numberOfPlaces = 2.0
+            let multiplier = Double(pow(10.0, numberOfPlaces))
+            let floored = String(floor(newBill! * multiplier) / multiplier)
+            print(floored)
+            billAmount = floored
+        } else {
+            billAmount = build
+        }
+        updateUI()
     }
-    
+    @IBAction func changeTipPerc(_ sender: UISlider) {
+        var currTipPerc = Int(sender.value)
+        print("Tip Percentage changing to \(currTipPerc)")
+    }
+    func updateUI() {
+        billAmt.text = billAmount
+    }
 }
 
